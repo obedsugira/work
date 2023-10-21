823d1312208e88917cecf86e5aa4521a55972fbc
obedi <obedisugira@gmail.com>
2023-10-21 19:08:58 +0200

    my first react app to change clrs

---
+++ project/src/App.js
@@ -0,0 +1,16 @@
+
+import './app1/App1.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+      <p id="class1" >
+        my name is celestin x chiva
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
